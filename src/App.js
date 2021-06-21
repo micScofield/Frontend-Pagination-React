68ce43f1fc511bb3f4c39032dc25c3936b5f5ca2
Sanyam Jain <sanyamjaino98@gmail.com>
2021-06-22 00:38:16 +0530

    Pagination fixed

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ const App = () => {
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(false)
     const [currentPage, setCurrentPage] = useState(1)
-    const [postsPerPage] = useState(10)
+    const [postsPerPage] = useState(7)
 
     useEffect(() => {
         const fetchPosts = async () => {
+            console.log('making request')
             setLoading(true)
             const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
             setPosts(res.data)
@@ -24,15 +25,18 @@ const App = () => {
     // Get current posts
     const indexOfLastPost = currentPage * postsPerPage
     const indexOfFirstPost = indexOfLastPost - postsPerPage
+
     const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
 
-    // Change page
+    // Change page number
     const paginate = pageNumber => setCurrentPage(pageNumber)
 
     return (
         <div className='container mt-5'>
             <h1 className='text-primary mb-3'>My Blog</h1>
+
             <Posts posts={currentPosts} loading={loading} />
+
             <Pagination
                 postsPerPage={postsPerPage}
                 totalPosts={posts.length}
